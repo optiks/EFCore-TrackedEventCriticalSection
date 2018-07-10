f46c971f560335c0b782da90e723257beff7baa7
Michael Chandler <michael.chandler@readify.net>
2018-07-10 15:27:05 +1000

    Initial commit.

---
+++ DataContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCore_TrackedEventCriticalSection
+{
+    public class TestDataContext : DbContext
+    {
+        public TestDataContext(DbContextOptions options) : base(options)
+        {
+            this.ChangeTracker.Tracked += ChangeTracker_Tracked;
+        }
+
+        private void ChangeTracker_Tracked(object sender, Microsoft.EntityFrameworkCore.ChangeTracking.EntityTrackedEventArgs e)
+        {
+            if (e.Entry.Entity is Employee employee)
+            {
+                var devices = employee.Devices;
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeDevice>()
+                .HasOne<Employee>(a => a.Employee)
+                .WithMany(p => p.Devices)
+                .HasForeignKey(a => a.EmployeeId)
+                .IsRequired(true);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<EmployeeDevice> Devices { get; set; }
+    }
+
+    public class EmployeeDevice
+    {
+        [Key]
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public string Device { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.SqlServerCe;
+using System.Linq;
+
+namespace EFCore_TrackedEventCriticalSection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ceConnectionString = "Data Source=TestDb.sdf; Persist Security Info = False; ";
+            var ceConnection = new SqlCeConnection(ceConnectionString);
+            ceConnection.Open();
+
+            var options = new DbContextOptionsBuilder<TestDataContext>()
+                .UseSqlCe(ceConnection)
+                .UseLazyLoadingProxies()
+                .Options;
+
+            var context = new TestDataContext(options);
+            var employees = context.Set<Employee>().ToList();
+        }
+    }
+}
